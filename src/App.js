204bdbe5ae89d6ba612518ebb6cd0048342e8209
kawgh1 <krseven@gmail.com>
2021-10-21 04:58:07 -0500

    add faqs container, faqs component and opt-form component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
-import jumboData from "./fixtures/jumbo.json";
 import { JumbotronContainer } from "./containers/jumbotron";
+import { FooterContainer } from "./containers/footer";
+import { FaqsContainer } from "./containers/faqs";
 
 export default function App() {
-    return <JumbotronContainer />;
+    return (
+        <>
+            <JumbotronContainer />
+            <FaqsContainer />
+            <FooterContainer />
+        </>
+    );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,10 @@
 // this index is our reference to all our components that we create
 // allowing us to call any component from this one file
 
+// think of as a small 'Component Library'
+
 export { default as Jumbotron } from "./jumbotron";
+
+export { default as Footer } from "./footer";
+export { default as Accordion } from "./accordion";
+export { default as OptForm } from "./opt-form";
