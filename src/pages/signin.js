c656ac9cdb00d26d88bf357e4f2bd69334e2f6f4
kawgh1 <krseven@gmail.com>
2021-10-22 10:48:49 -0500

    styled sign in component

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,86 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { FirebaseContext } from "../context/firebase";
+import { Form } from "../components";
+import { HeaderContainer } from "../containers/header";
+import { FooterContainer } from "../containers/footer";
+import * as ROUTES from "../constants/routes";
 
-export default function Signin() {
-    return <p>Hello from Sign in</p>;
+export default function SignIn() {
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+
+    const [emailAddress, setEmailAddress] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const isInvalid = password === "" || emailAddress === "";
+
+    const handleSignin = (event) => {
+        event.preventDefault();
+
+        return firebase
+            .auth()
+            .signInWithEmailAndPassword(emailAddress, password)
+            .then(() => {
+                history.push(ROUTES.BROWSE);
+            })
+            .catch((error) => {
+                setEmailAddress("");
+                setPassword("");
+                setError(error.message);
+            });
+    };
+
+    return (
+        <>
+            <HeaderContainer>
+                <Form>
+                    <Form.TextSmall style={{ color: "lime" }}>
+                        For demo purposes user can enter
+                        username="test@gmail.com" and password="test"
+                    </Form.TextSmall>
+                    <Form.Title>Sign In</Form.Title>
+
+                    {error && (
+                        <Form.Error data-testid="error">{error}</Form.Error>
+                    )}
+
+                    <Form.Base onSubmit={handleSignin} method="POST">
+                        <Form.Input
+                            placeholder="Email address"
+                            value={emailAddress}
+                            onChange={({ target }) =>
+                                setEmailAddress(target.value)
+                            }
+                        />
+                        <Form.Input
+                            type="password"
+                            value={password}
+                            autoComplete="off"
+                            placeholder="Password"
+                            onChange={({ target }) => setPassword(target.value)}
+                        />
+                        <Form.Submit
+                            disabled={isInvalid}
+                            type="submit"
+                            data-testid="sign-in"
+                        >
+                            Sign In
+                        </Form.Submit>
+                    </Form.Base>
+
+                    <Form.Text>
+                        New to Netflix?{" "}
+                        <Form.Link to="/signup">Sign up now.</Form.Link>
+                    </Form.Text>
+                    <Form.TextSmall>
+                        This page is protected by Google reCAPTCHA to ensure
+                        you're not a bot. Learn more.
+                    </Form.TextSmall>
+                </Form>
+            </HeaderContainer>
+            <FooterContainer />
+        </>
+    );
 }
