8c4d6c350ff8a30be36f8727da14cff95f401551
kawgh1 <krseven@gmail.com>
2021-10-22 10:19:16 -0500

    set up firebase database, seeded database, set up firebase context

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom";
 import "normalize.css";
 import App from "./app";
 import { GlobalStyles } from "./global-styles";
+// only used this once to seed database with raw data
+import { firebaseApp } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 
 ReactDOM.render(
     <>
-        <GlobalStyles />
-        <App />
+        <FirebaseContext.Provider value={{ firebaseApp }}>
+            <GlobalStyles />
+            <App />
+        </FirebaseContext.Provider>
     </>,
     document.getElementById("root")
 );
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,13 +1,27 @@
 // prod = production version
-import Firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+// v9 compat packages are API compatible with v8 code
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+// only run this seeDatabase **once** or it will duplicate data in firestore
+// import { seedDatabase } from "../seed";
 
 // we need to seed the database
 
 // config
-const config = {};
+const firebaseConfig = {
+    apiKey: "AIzaSyByMkbq26uxU1L_TKgCmNLiuZKRrm7D3jQ",
+    authDomain: "netflix-clone-v2-31e72.firebaseapp.com",
+    projectId: "netflix-clone-v2-31e72",
+    storageBucket: "netflix-clone-v2-31e72.appspot.com",
+    messagingSenderId: "1025552307279",
+    appId: "1:1025552307279:web:ee25e46d347457f3e9c63a",
+};
 
-const firebase = Firebase.initializeApp(config);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-export { firebase };
+// Never uncomment this or it will re-seed the database with 500 lines of duplicate code
+/////////////
+//////////////////// seedDatabase(firebaseApp);
+//////////////
+export { firebaseApp };
